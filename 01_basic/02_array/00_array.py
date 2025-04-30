dcb2802edcd4e93e8426551def5c372878723907
joohee-sailingstone <92443218+woolfie1101@users.noreply.github.com>
2025-04-30 15:58:54 +0900

    정렬 - 거품정렬

--- 01_basic/02_array/00_array.py
+++ 01_basic/02_array/00_array.py
@@ -12,6 +12,7 @@ n=len(arr)
 for i in range(n):
     print(i)
 
+arr = [3,1,4,5,2]
 # 선택 정렬
 def selection_sort(arr):
     n = len(arr)
@@ -25,5 +26,15 @@ def selection_sort(arr):
     return arr
 print(selection_sort(arr))
 
-
+arr = [3,1,4,5,2]
+# 거품 정렬
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if arr[j + 1] < arr[j]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                print(f'버블 정렬 변경 리스트 = {arr}')
+    return arr
+print(f'버블 정렬 : {bubble_sort(arr)}')
 
